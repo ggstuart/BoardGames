78455fe662e5e0f01ffa0c887eabca537c9d74bf
Graeme Stuart <ggstuart@gmail.com>
2010-05-06 22:21:25 +0200

    Delete this for now. Need to package the library up properly first.

--- GameOfLife/test.rb
+++
@@ -1,6 +0,0 @@
-require '~/Code/Ruby/BoardGames/Base'
-require '~/Code/Ruby/BoardGames/GameOfLife/GameOfLife'
-require '~/Code/Ruby/BoardGames/GameOfLife/CLI'
-
-cli = BoardGames::GameOfLife::CLI.new
-cli.run!
