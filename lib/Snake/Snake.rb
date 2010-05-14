7d67c4cb666d7e660ba7707e34804975725e6fca
Graeme Stuart <ggstuart@gmail.com>
2010-05-14 10:16:44 +0200

    Upgrade so snake can be controlled

--- lib/Snake/Snake.rb
+++ lib/Snake/Snake.rb
@@ -2,33 +2,30 @@ module BoardGames
 
   #Snake is simple but needs careful thought
   #A snake is a list of cells
-  #the snake cell must apear different when it's part of a snake
+  #the snake cell must be different when it's part of a snake
   #So the cell should know about any snake they are part of.
+  #The cell detects collisions and tells the snake to lose a life
+  #control via keypress stuff is handled in the interface
 
   module Snake
+    LEFT = 1
+    RIGHT = 2
+    UP = 3
+    DOWN = 4
     
     #a snake is implemented as an array of cells acting as a filo stack
     #Snakes know about each other in class veriables
     
     class Snake
-
-      LEFT = 1
-      RIGHT = 2
-      UP = 3
-      DOWN = 4
-      
-      def initialize(head, length = 4, symbol = '*', head_symbol = 'X', direction = UP)
+      def initialize(head, length = 4, direction = UP)
         @length = length
         head.snake = self
         @snake = [head]
-        @symbol = symbol
-        @head_symbol = head_symbol
         @direction = direction
       end
       
-      #to start with, just move it left
       def move
-        head = next_head#random#@snake[0].left
+        head = next_head
         head.snake = self
         @length += head.food
         head.food = 0
@@ -41,25 +38,25 @@ module BoardGames
       def includes?(cell)
         @snake.includes?(cell)
       end
-      
-      def to_s(cell)
-        cell == head ? @head_symbol : @symbol
-      end
-      
-      def turn
-        @direction = [LEFT, RIGHT, UP, DOWN].delete_if{|dir| dir == back}[rand(3)]
+            
+      def turn(direction = nil)
+        @direction = direction || [LEFT, RIGHT, UP, DOWN].delete_if{|dir| dir == back}[rand(3)]
       end
 
       def grow
         @length += rand(1)
       end
-      
-      private
+
+      def to_s
+        "0#{(@length-1)*'#'}"
+      end
 
       def head
         @snake[0]
       end
       
+      private
+            
       def next_head
         h = @snake[0]
         case @direction
@@ -100,7 +97,7 @@ module BoardGames
     class Session
       attr :x
       attr :y
-      def initialize(x = 25, y = 15, snake_count = 15)
+      def initialize(x = 25, y = 15, snake_count = 1)
         @x = x
         @y = y
         @snake_count = snake_count
@@ -131,31 +128,23 @@ module BoardGames
 
       def iterate!
         @snakes.each do |snake|
-          snake.turn if rand(10) >= 5
           snake.move
         end
+      end
+
+      def turn(direction = nil)
+        @snakes[0].turn(direction)
       end
 
       def iterate
         while true
           @snakes.each do |snake|
-            snake.turn if rand(10) >= 5
             snake.move
             yield @board.to_s
           end
         end
       end
       
-#      def run
-#        puts @board
-#        200.times do
-#          sleep 0.2
-#          @snake.turn if rand(10) >= 5
-#          @snake.move
-#          puts @board
-#        end
-#      end
-
       def to_s
         @board.to_s
       end
@@ -166,10 +155,6 @@ module BoardGames
       def clear
         @data = (0..size).collect{Cell.new(self)}
         addFood(10)
-      end
-
-      def randomCell
-        self[rand(@x), rand(@y)]
       end
 
       def addFood(n)
@@ -198,7 +183,11 @@ module BoardGames
         end
         
         def to_s
-          @snake.nil? ? @food > 0 ? @food.to_s : '.' : @snake.to_s(self)
+          if !@snake 
+            (@food > 0 ? @food.to_s : '.')
+          else
+            (@snake.head == self ? '0' : '#')
+          end
         end
         
         def occupied?
